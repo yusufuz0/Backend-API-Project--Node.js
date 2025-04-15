c5fed4c655241c54820f27ae8447c2281d136208
Yusuf <133169287+yusufuz0@users.noreply.github.com>
2025-04-15 16:33:02 +0300

    jwt token güvenliği için secret key güncellemesi.

--- api/config/index.js
+++ api/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
     "PORT": process.env.PORT || "3000",
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
     "JWT": {
-        "SECRET":"123456",
+        "SECRET": process.env.JWT_SECRET || "sdflmsdflmsdlfişdsfilsdilfs66s456d4f56s465df465sdhjkhg4fsdlfkmçkmsdkjgs179179asdghjkhsdfgdsfgdsfgfds46g456ds4fg65sd2f1g3d54fg848hjg5hjgh534j6fghn32vb1n53fh4j65fhhn32fghjgjkcbntgdf8797f65sdd1v2sa68df4g",
         "EXPIRE_TIME": !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME)) ? parseInt(process.env.TOKEN_EXPIRE_TIME) : 24 * 60 * 60 // 86400
         
     },
